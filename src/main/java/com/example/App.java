f2628712271b33e17555ad8e8d733e1be23ea58e
praveenthati23 <134906977+praveenthati23@users.noreply.github.com>
2025-11-05 22:01:38 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Azure VM! - Version 1");
+        System.out.println("Hello, Azure VM! - Version 2 is the latest update");
     }
 }
